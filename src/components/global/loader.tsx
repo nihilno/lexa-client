596e62376404eee3bf2b8af0e20bedaf758d30a8
Maciej <maciej.polowy1@gmail.com>
2026-02-16 11:00:53 +0100

    feat: add loading message with fade-in animation to enhance user feedback during API loading

--- src/components/global/loader.tsx
+++ src/components/global/loader.tsx
@@ -1,9 +1,25 @@
 import { Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
 
 function Loader() {
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowMessage(true);
+    }, 4000);
+
+    return () => clearTimeout(timer); // cleanup on unmount
+  }, []);
+
   return (
     <div className="center absolute">
       <Loader2 className="animate-spin" />
+      {showMessage && (
+        <p className="animate-fadeIn opacity-0">
+          First load may take a couple of seconds, since API is dormant.
+        </p>
+      )}
     </div>
   );
 }
